6d5ca24c162c83cbaa6cd03c4f4bf2709972ffbe
Pepelu <Pepelu@DESKTOP-6L34ONV>
2021-02-03 01:13:35 +0100

    editado clase salario

--- EjercicioJUnit2/src/Salario.java
+++ EjercicioJUnit2/src/Salario.java
@@ -1,13 +1,10 @@
-
 public class Salario {
 
-		
+
 	Salario(){
-		
+	
 		}
 	
-	
-
 	
 	public static float calculaSalarioBruto(String tipo, float ventasMes,float horasExtra) {
 		float salario=0;
@@ -38,19 +35,67 @@ public class Salario {
 		
 	}
 	public static float calcularSalarioNeto(float salarioBruto) {
-		float salBru=0;
+		float salBru=salarioBruto;
 		
 			if (salarioBruto>=1000 && salarioBruto<1500) {
-				salBru=salarioBruto-(salarioBruto*(16/100));
-			} else {
-				if (salarioBruto>=1500) {
-					salBru=salarioBruto-(salarioBruto*(18/100));
-				} else {
-					salBru=salarioBruto;
-				}
+				salBru=(float) (salarioBruto-(salarioBruto*0.16));
+			}
+			if (salarioBruto>=1500) {
+				salBru=(float) (salarioBruto-(salarioBruto*0.18));
+				
 			}
 		
 		return salBru;
 		
 	}
+}public class Salario {
+
+
+	Salario(){
+	
+		}
+	
+	
+	public static float calculaSalarioBruto(String tipo, float ventasMes,float horasExtra) {
+		float salario=0;
+		float salarioBase=0;
+		float prima=0;
+		float horas=0;
+		if (tipo.equals("vendedor")) {
+			salarioBase=1000;
+		}else {
+			if (tipo.equals("encargado")) {
+				salarioBase=1500;
+			} else {
+				System.out.println("no reconozco el tipo");
+			} 
+		}
+		if (ventasMes>=1000 && ventasMes<1500) {
+			prima=100;
+		}else {
+			if (ventasMes>=1500) {
+				prima=200;
+			} else {
+				prima=0;
+			}
+		}
+		horas=horasExtra*20;
+		salario=salarioBase+prima+horas;
+		return salario;
+		
 	}
+	public static float calcularSalarioNeto(float salarioBruto) {
+		float salBru=salarioBruto;
+		
+			if (salarioBruto>=1000 && salarioBruto<1500) {
+				salBru=(float) (salarioBruto-(salarioBruto*0.16));
+			}
+			if (salarioBruto>=1500) {
+				salBru=(float) (salarioBruto-(salarioBruto*0.18));
+				
+			}
+		
+		return salBru;
+		
+	}
+}
